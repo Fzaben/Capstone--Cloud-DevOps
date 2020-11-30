23f61da36f795082edbcde0790186f34f69809c5
faisalzaben <fzaben@terkwaz.com>
2020-11-30 21:24:17 +0200

    check jenkines integration

--- scripts/integration/build.sh
+++ scripts/integration/build.sh
@@ -3,6 +3,5 @@ export TAG=$BUILD_NUMBER
 TAG=$BUILD_NUMBER docker-compose -f pipeline/build.yml build $APP_NAME
 TAG=latest docker-compose -f pipeline/build.yml build $APP_NAME
 
-docker login --username $DOCKER_HUB_USERNAME --password $DOCKER_HUB_PASSWORD
 TAG=$BUILD_NUMBER docker-compose -f pipeline/build.yml push $APP_NAME
 TAG=latest docker-compose -f pipeline/build.yml push $APP_NAME
